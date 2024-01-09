abc26fc8b2d70eb3b22a6f9846e032977bf2ed49
webnai <wilbertboadzo144@gmail.com>
2024-01-09 20:39:58 +0000

    3d canvas added

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSnapshot } from "valtio";
 
-const Tab = () => {
-  return <div>Tab</div>;
+import state from "../store";
+
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${isFilterTab} ? 'rounded-full
+     : 'rounded-4'}`}
+    ></div>
+  );
 };
 
 export default Tab;
